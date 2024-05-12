6882542072dd50612ddc831b9a3cfe9aab47f3db
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-05-12 15:32:08 +0000

    done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,4 +33,40 @@ function loginUserMessage(username="sam") {
 }
 
 // console.log(loginUserMessage("gaurav"))
-console.log(loginUserMessage("gaurav"))
+// console.log(loginUserMessage("gaurav"))
+
+function calculateCartPrice(val1,val2,...num1)
+{
+    return num1
+}
+
+// ... -> rest operator
+
+// console.log(calculateCartPrice(200,300,400,2000));
+
+const user = {
+    username : "gaurav",
+    prices : "24567"
+}
+
+function handleObject(anyobject)
+{
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username : "sam",
+    price : 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray) {
+    return getArray[3]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,600]));
+
