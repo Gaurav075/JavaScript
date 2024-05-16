3a0eb2247200d33d0283da5196469f2127201a04
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-05-16 14:08:53 +0000

    done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,8 +5,50 @@ if(true)
     {
         let a=20
         const b = 30
-        console.log("inner:",a);
+        // console.log("inner:",a);
     }
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one()
+{
+    const username = "Gaurav"
+
+    function two()
+    {
+        const website = "YouTube"
+        console.log(username);
+
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true)
+    {
+        const username= "Gaurav"
+        if(username == "Gaurav")
+            {
+                const website = "YouTube"
+                // console.log(username+website);
+            }
+            // console.log(website);
+    }
+    // console.log(username);
+
+    // ******************** Interesting ********************
+    
+    console.log(addone(5))
+    function addone(num)
+    {
+        return num+1
+    }
+
+
+    addtwo(5)  // error aa jayega isme
+    const addtwo = function(num){
+
+        return num+2
+    }
