ab672f547401c551713dffbb4f79e3326309aa2e
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-05-20 14:25:27 +0000

    done

---
+++ 05_interations/two.js
@@ -0,0 +1,19 @@
+
+// let index = 0;
+// while (index<=10) {
+//     console.log(`Value of index is ${index}`);
+//     index=index+2
+// }
+
+// let myarray = ['flash','batman','superman']
+// let i=0;
+// while(i<myarray.length){
+//     console.log(`${myarray[i]}`);
+//     i++
+// }
+
+let score=11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
